482af1cf384098cd98ae2bceeed554342480a5f5
jiapeish <jiapeish@gmail.com>
2025-07-06 15:12:39 +0800

    parse rolls

--- maku_python_homework_02.py
+++ maku_python_homework_02.py
@@ -8,12 +8,76 @@
 
 ## Reference
 ## https://www.topendsports.com/sport/tenpin/scoring.htm
+## https://en.wikipedia.org/wiki/Tenpin_bowling
 ## https://www.activesgcircle.gov.sg/learn/bowling/how-are-points-determined-in-bowling
 ## Use this calculator to verify: https://www.bowlinggenius.com/
 ##
 
 ## Frame      1   2   3   4   5   6   7   8   9   10       Score
-##            X   X   X   X   X   X   X   X   X   X X X    300
+##            X   X   X   X   X   X   X   X   X   XXX      300
 ##            1-  1-  1-  1-  1-  1-  1-  1-  1-  1-       10
 ##            X   9/  5/  72  X   X   X   9-  8/  9/X      187
 ##            5/  5/  5/  5/  5/  5/  5/  5/  5/  5/5      150
+
+import re
+
+def get_rolls(game):
+    frames = re.split(r'\s+', game.strip())
+    rolls = []
+
+    for i, frame in enumerate(frames):
+        if i < 9:
+            if frame == 'X':
+                rolls.append(10)
+            else:
+                first, second = frame[0], frame[1]
+                if first == '-':
+                    roll_first = 0
+                else:
+                    roll_first = int(first)
+
+                if second == '/':
+                    roll_second = 10 - roll_first
+                elif second == '-':
+                    roll_second = 0
+                else:
+                    roll_second = int(second)
+
+                rolls.extend([roll_first, roll_second])
+        else:
+            for roll in frame:
+                if roll.isdigit():
+                    rolls.append(int(roll))
+                    already = int(roll)
+                elif roll == 'X':
+                    rolls.append(10)
+                    already = 10
+                elif roll == '-':
+                    rolls.append(0)
+                    already = 0
+                elif roll == '/':
+                    rolls.append(10 - already)
+
+    return rolls
+
+
+def main():
+    game1 = "X   X   X   X   X   X   X   X   X   X X X"
+    rolls1 = get_rolls(game1)
+    print("rolls1:", rolls1)
+
+    game2 = "1-  1-  1-  1-  1-  1-  1-  1-  1-  1-"
+    rolls2 = get_rolls(game2)
+    print("rolls2:", rolls2)
+
+    game3 = "X   9/  5/  72  X   X   X   9-  8/  9/X"
+    rolls3 = get_rolls(game3)
+    print("rolls3:", rolls3)
+    
+    game4 = "5/  5/  5/  5/  5/  5/  5/  5/  5/  5/5"
+    rolls4 = get_rolls(game4)
+    print("rolls4:", rolls4)
+
+
+if __name__ == "__main__":
+    main()
