98dadf8d5685c623beb0bbfaa7b733fadad96608
jiapeish <jiapeish@gmail.com>
2025-07-06 11:06:21 +0800

    understand hw 02

---
+++ maku_python_homework_02.py
@@ -0,0 +1,19 @@
+## 为了表示这个Bowling游戏，我们做如下约定，该约定与 “维基百科-保龄球-记分方式-传统方式” 的约定一致
+## https://zh.wikipedia.org/wiki/%E4%BF%9D%E9%BD%A1%E7%90%83
+## 1. 一局游戏由10轮组成，每轮有2次投球机会，每轮游戏之间使用空格分割；
+## 2. 如果第一次投球击倒10个瓶子，记为X，该轮结束；
+## 3. 如果第一次投球没有击倒10个瓶子，第二次击倒所有剩余的瓶子，则记为第一次的击倒瓶子数量和/；如 7/
+## 4. 如果第一次投球没有击倒10个瓶子，第二次击倒部分剩余的瓶子，则记为第一次的击倒瓶子数量和第二次击倒瓶子数量；如42
+## 5. 如果一次击球没有击倒瓶子，则记为-；如1-
+
+## Reference
+## https://www.topendsports.com/sport/tenpin/scoring.htm
+## https://www.activesgcircle.gov.sg/learn/bowling/how-are-points-determined-in-bowling
+## Use this calculator to verify: https://www.bowlinggenius.com/
+##
+
+## Frame      1   2   3   4   5   6   7   8   9   10       Score
+##            X   X   X   X   X   X   X   X   X   X X X    300
+##            1-  1-  1-  1-  1-  1-  1-  1-  1-  1-       10
+##            X   9/  5/  72  X   X   X   9-  8/  9/X      187
+##            5/  5/  5/  5/  5/  5/  5/  5/  5/  5/5      150
