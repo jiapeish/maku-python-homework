fc573e6c0a064a8a5599fa7c35001e291b0ca816
jiapeish <jiapeish@gmail.com>
2025-07-06 09:51:03 +0800

    add stage 2: new requirements

--- maku_python_homework_01.py
+++ maku_python_homework_01.py
@@ -14,9 +14,24 @@ class FizzBuzz():
             else:
                 print(i)
 
+    def play_stage_2(self):
+        for i in range(self.start, self.end + 1):
+            if (i % 3 == 0 or '3' in str(i)) and (i % 5 == 0 or '5' in str(i)):
+                print("FizzBuzz")
+            elif i % 3 == 0 or '3' in str(i):
+                print("Fizz")
+            elif i % 5 == 0 or '5' in str(i):
+                print("Buzz")
+            else:
+                print(i)
+
 def main():
     game = FizzBuzz(1, 100)
     game.play()
+
+    print("--------------------------------")
+
+    game.play_stage_2()
 
 if __name__ == "__main__":
     main()
